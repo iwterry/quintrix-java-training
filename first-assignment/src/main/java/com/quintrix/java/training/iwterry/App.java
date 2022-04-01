4f3826edb3b4c9a17261c4b8f02682018b5f8b7f
Isaac Terry <zeroice2007@gmail.com>
2022-04-01 18:37:34 -0500

    Add an example of autoboxing/unboxing while creating a git branch

--- first-assignment/src/main/java/com/quintrix/java/training/iwterry/App.java
+++ first-assignment/src/main/java/com/quintrix/java/training/iwterry/App.java
@@ -15,5 +15,12 @@ public class App
         } else {
             System.out.println("The sum is not greater than 20.");
         }
+
+        Double doubleWrapper = Double.valueOf("3.14");
+        double doublePrimitive = 3.14;
+
+        if (doublePrimitive == doubleWrapper) {
+            System.out.println("this is an example of concept autoboxing/unboxing");
+        }
     }
 }
